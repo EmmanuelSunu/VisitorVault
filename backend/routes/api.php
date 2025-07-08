b1f8f263bffbeb8db226b7bc51310bd817954ef3
Severin Asutani <50738523+Awedoba@users.noreply.github.com>
2025-07-08 01:23:16 +0000

    add back end and  create sup folders for bakn and front end

---
+++ backend/routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\VisitorController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+
+Route::post('/login', [UserController::class, 'login']);
+Route::post('/register', [UserController::class, 'register']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [UserController::class, 'logout']);
+    Route::get('/me', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::apiResource('/visitor', VisitorController::class);
+});
